d5bc668a1c1651a6d0dead5b44c544b8892be241
rashid30111998 <kalathingalrashid89@gmail.com>
2023-07-12 15:28:10 +0530

    initial commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,53 +1,73 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import './Header.css';
 import OlxLogo from '../../assets/OlxLogo';
 import Search from '../../assets/Search';
 import Arrow from '../../assets/Arrow';
 import SellButton from '../../assets/SellButton';
 import SellButtonPlus from '../../assets/SellButtonPlus';
-function Header() {
+import { AuthContext, FirebaseContext } from '../../store/Context';
+
+const Header = () => {
+  const history = useHistory();
+  const { user } = useContext(AuthContext);
+  const { firebase } = useContext(FirebaseContext);
+
+  const handleLogin = () => {
+    history.push('/login');
+  };
+
+  const handleLogout = () => {
+    firebase.auth().signOut();
+    history.push('/login');
+  };
+
+  const handleSell = () => {
+    history.push('/create');
+  };
+
   return (
     <div className="headerParentDiv">
       <div className="headerChildDiv">
         <div className="brandName">
-          <OlxLogo></OlxLogo>
+          <OlxLogo />
         </div>
         <div className="placeSearch">
-          <Search></Search>
+          <Search />
           <input type="text" />
-          <Arrow></Arrow>
+          <Arrow />
         </div>
         <div className="productSearch">
           <div className="input">
-            <input
-              type="text"
-              placeholder="Find car,mobile phone and more..."
-            />
+            <input type="text" placeholder="Find car, mobile phone and more..." />
           </div>
           <div className="searchAction">
-            <Search color="#ffffff"></Search>
+            <Search color="#ffffff" />
           </div>
         </div>
         <div className="language">
           <span> ENGLISH </span>
-          <Arrow></Arrow>
+          <Arrow />
         </div>
         <div className="loginPage">
-          <span>Login</span>
+          <span onClick={user ? handleLogout : handleLogin}>{user ? `Welcome ${user.displayName}` : 'Login'}</span>
           <hr />
         </div>
-
-        <div className="sellMenu">
-          <SellButton></SellButton>
+        {user && (
+          <span onClick={handleLogout}>
+            Logout
+          </span>
+        )}
+        <div className="sellMenu" onClick={handleSell}>
+          <SellButton />
           <div className="sellMenuContent">
-            <SellButtonPlus></SellButtonPlus>
+            <SellButtonPlus />
             <span>SELL</span>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Header;
